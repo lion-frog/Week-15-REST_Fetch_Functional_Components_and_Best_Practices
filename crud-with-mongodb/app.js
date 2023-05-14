77b1889a032d95ddedb68450ebf7c80dec90f46b
Your Name <you@example.com>
2023-05-13 23:07:45 -0400

    project update

---
+++ crud-with-mongodb/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+//middleware
+app.use(express.json());
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
+
+module.exports = app;
+
+const mongoose = require("mongoose");
+//configure mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
+
+const blogRouter = require("./routes/BlogRoutes");
+
+app.use("/api/blogs", blogRouter);
